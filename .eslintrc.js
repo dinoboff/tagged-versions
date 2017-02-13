aa781b37887ffa5e3883a987ccf07d511b6d19b7
Damien Lebrun <dinoboff@hotmail.com>
2017-02-13 16:42:42 +0000

    chore: update dev dependencies
    Mainly Ava 0.18 provide string and object diff.
    
    The common-dangle rule had to be tweak, since dangle comma for function parameter is not supported by Node yet.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,15 @@ module.exports = {
         tabWidth: 2,
       },
     ],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
   },
 };
